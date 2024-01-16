b2fcc1ce547ac26156d30544c8c7f87f20d172b2
Omkar R <40696201+omatography@users.noreply.github.com>
2024-01-16 22:03:38 +0530

    testng integrated

--- src/test/java/org/omatography/AdvancedJavaWebAutomationFramework/testRunners/MyTestRunnerTestNG.java
+++ src/test/java/org/omatography/AdvancedJavaWebAutomationFramework/testRunners/MyTestRunnerTestNG.java
@@ -1,15 +1,23 @@
 package org.omatography.AdvancedJavaWebAutomationFramework.testRunners;
 
 import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 import org.junit.runner.RunWith;
+import org.testng.annotations.DataProvider;
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src\\test\\resources\\appFeatures\\login.feature"},
         glue = {"org.omatography.AdvancedJavaWebAutomationFramework.stepDefinitions",
                 "org.omatography.AdvancedJavaWebAutomationFramework.appHooks"},
         plugin = {"pretty"}
 )
-public class MyTestRunner {
+public class MyTestRunnerTestNG extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider
+    public Object [][] scenarios(){
+        return super.scenarios();
+    }
 }
